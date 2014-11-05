9902edaaf8da4ac6508521d8506a8c46ba745272
konrad_kory_ski <kondzio6@gmail.com>
2014-11-05 19:26:30 +0100

    logowanie

--- Passdroid/src/pl/dkdeveloper/passdroid/MainActivity.java
+++ Passdroid/src/pl/dkdeveloper/passdroid/MainActivity.java
@@ -1,17 +1,28 @@
 package pl.dkdeveloper.passdroid;
 
+import pl.dkdeveloper.logic.AuthenticateManager;
+import pl.dkdeveloper.logic.AuthenticationResult;
+import pl.dkdeveloper.logic.LoginResultEnum;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	TextView tbPassword;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		tbPassword = (TextView)findViewById(R.id.tbPassword);
 		
 		this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
 	}
@@ -34,4 +45,25 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	public void btnLogin_onClick(View view) {
+		
+		if (tbPassword.getText().length() > 0) {
+			AuthenticateManager manager = new AuthenticateManager();
+			AuthenticationResult result = manager.Authenticate(tbPassword.getText().toString());
+			
+			if(result.IsSuccess && result.LoginResult == LoginResultEnum.SuccesS) {
+				// we can put message as reult in intent
+				Intent intent = new Intent(this, CategoryActivity.class);		
+				startActivity(intent);
+			}
+			else if (result.IsSuccess && result.LoginResult == LoginResultEnum.FakePassword) {
+				// open fake category activity or put message in intent
+			}
+		}
+		else {
+			Toast.makeText(this, "Wrong password!", Toast.LENGTH_LONG);
+		}
+	}
 }
+
